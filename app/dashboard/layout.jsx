ab4a0f48e8663f921e185596a9154e36ac2277c7
Amalroy212 <ken940005@gmail.com>
2023-12-28 11:22:12 +0530

    fixed

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,5 +1,5 @@
 import Sidebar from '@/app/ui/dashboard/sidebar/Sidebar'
-import Navbar from '@/app/ui/dashboard/navbar/navbar';
+import Navbar from '@/app/ui/dashboard/navbar/Navbar';
 import styles from "@/app/ui/dashboard/dashboard.module.css";
 import Footer from '@/app/ui/dashboard/footer/Footer';
 
