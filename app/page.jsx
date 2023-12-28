9f98be1a69c030c3090e6c59ba64fa3084cdbb7a
Amalroy212 <ken940005@gmail.com>
2023-12-28 11:37:37 +0530

    link

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello</h1>
+      <Link href="/login">LOGIN</Link>
     </main>
   )
 }
