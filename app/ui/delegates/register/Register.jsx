a03230df434ecde05abfdf0066466a346c9cffa5
suhail-mohd07 <suhailkudige786@gmail.com>
2024-02-20 12:07:36 +0530

    New delegate registration button

--- app/ui/delegates/register/Register.jsx
+++ app/ui/delegates/register/Register.jsx
@@ -48,8 +48,8 @@ function Register({ error, setError, setRegister }) {
       <h1 className='absolute top-10 text-[22px] bg-[#182237] p-5 rounded-2xl flex gap-3 items-center'>You already have registered mail id ?? <button className='flex items-center gap-2 text-[15px] bg-green-700 py-1 px-3 rounded' onClick={() => setRegister(false)}><FaArrowAltCircleRight />Verify</button></h1>
       <form onSubmit={handleCheckin} className="w-full md:w-[50%] h-auto p-10 bg-[#182237] rounded-2xl relative flex justify-center items-center flex-col">
         <img className='absolute top-[-1.5rem]' src="https://omandits.com/assets/images/dits.png" width={50} height={50} alt="" />
-        <h1 className="absolute top-10">Delegate Form</h1>
-        <div className='w-full h-auto md:flex-row flex-col flex gap-5 justify-center items-center md:mt-10 mt-[5rem]'>
+        {/* <h1 className="absolute top-10">Delegate Form</h1> */}
+        {/* <div className='w-full h-auto md:flex-row flex-col flex gap-5 justify-center items-center md:mt-10 mt-[5rem]'>
           <input required className='w-full p-2 border rounded-xl bg-[#151c2c]' type="text" name="fname" id="fname" placeholder='First name' />
           <input required className='w-full p-2 border rounded-xl bg-[#151c2c]' type="text" name="lname" id="lname" placeholder='Last name' />
         </div>
@@ -58,7 +58,9 @@ function Register({ error, setError, setRegister }) {
           <input required className='w-full p-2 border rounded-xl bg-[#151c2c]' type="text" name="job" id="job" placeholder='Job title' />
         </div>
         <input required className='w-full p-2 border rounded-xl bg-[#151c2c] md:mt-10 mt-6' type="companyName" name="companyName" id="companyName" placeholder='Company name' />
-        <button type='submit' className='p-2 mt-10 bg-green-500 px-10 rounded-2xl text-black'>Register</button>
+        <button type='submit' className='p-2 mt-10 bg-green-500 px-10 rounded-2xl text-black'>Register</button> */}
+        
+        <a href="https://omandits.com/delegateEnquiry.html" className=' py-2 px-5 rounded-lg bg-green-800 hover:border'>New Delegate Registration</a>
       </form>
     </div>
   )
